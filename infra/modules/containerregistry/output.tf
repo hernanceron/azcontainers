f44833e43b6422bf7a96505d98dcefda256ae24e
Hernan <hernanceron@gmail.com>
2024-11-14 16:26:56 -0500

    acr login server

--- infra/modules/containerregistry/output.tf
+++ infra/modules/containerregistry/output.tf
@@ -2,6 +2,10 @@ output "acrname" {
   value = azurerm_container_registry.acrproyecto.login_server
 }
 
+output "acrnamelogin" {
+  value = azurerm_container_registry.acrproyecto.name
+}
+
 output "serviceprincipalid" {
   value = data.azuread_service_principal.terraformHernan.id
 }
--- infra/output.tf
+++ infra/output.tf
@@ -1,3 +1,6 @@
 output "acrname" {
   value = module.azure_container_registry.acrname
 }
+output "acrnamelogin" {
+  value = module.azure_container_registry.acrnamelogin
+}
