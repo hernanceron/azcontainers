9bc6abf9d324177c170825e39558e1a0f1deab52
Hernan <hernanceron@gmail.com>
2024-11-14 23:58:12 -0500

    se agrego resource group name

--- infra/output.tf
+++ infra/output.tf
@@ -4,3 +4,6 @@ output "acrname" {
 output "acrnamelogin" {
   value = module.azure_container_registry.acrnamelogin
 }
+output "resource_group_name" {
+  value = azurerm_resource_group.rg-proyecto.name
+}
