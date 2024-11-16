6cdaa52e224270a9e4650da65c35ddc7d4c71183
Hernan <hernanceron@gmail.com>
2024-11-15 19:09:34 -0500

    se retiro azure container app

--- infra/modules/containerapp/output.tf
+++ infra/modules/containerapp/output.tf
@@ -1,6 +1,3 @@
 output "environment_name" {
   value = azurerm_container_app_environment.azure_container_environment.name
 }
-output "container_app_name" {
-  value = azurerm_container_app.container_app_proyecto.name
-}
